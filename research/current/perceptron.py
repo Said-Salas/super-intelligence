cbd9b50bc59cd09093b5b2731faf08c0610be5fe
Said Salas <saidsalastech@gmail.com>
2026-01-17 22:59:34 -0600

    SI 0.005

--- research/current/perceptron.py
+++ research/current/perceptron.py
@@ -8,4 +8,20 @@ class Perceptron:
     def activate(self, x):
         return 1 if x >= 0 else 0
 
+    def predict(self, inputs):
+        summation = np.dot(inputs, self.weights[1:]) + self.weights[0]
+        return self.activate(summation)
+
+    def train(self, inputs, target):
+        prediction = self.predict(inputs)
+        error = target - prediction
+        self.weights[0] += error * self.lr
+        self.weights[1:] += error * inputs * self.lr #does perceptron have synapses?
+
+        return error 
+
+#what can we teach to a perceptron?
+    
+    
+
     
