a26b00b0fb49d48021d98298f8e41887924afe35
Said Salas <saidsalastech@gmail.com>
2026-01-23 11:59:24 -0600

    SI 0.008

---
+++ research/current/digit_recognizer.py
@@ -0,0 +1,8 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
