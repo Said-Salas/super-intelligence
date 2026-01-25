9c4205197d818589f30eed9b4163644a98cd2863
Said Salas <saidsalastech@gmail.com>
2026-01-25 13:43:29 -0600

    SI 0.009

--- research/current/digit_recognizer.py
+++ research/current/digit_recognizer.py
@@ -6,3 +6,20 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5,), (0.5,))
 ])
+
+print("Downloading dataset...")
+train_set = torchvision.datasets.MNIST(
+    root='./data',
+    train=True,
+    download=True,
+    transform=transform
+)
+
+test_set = torchvision.datasets.MNIST(
+    root='./data',
+    train=False,
+    download=True,
+    transform=transform
+)
+
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=64, shuffle=True)
