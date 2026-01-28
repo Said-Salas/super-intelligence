7fb616a457a06c2bb95a42a91d5d71261f879a94
Said Salas <saidsalastech@gmail.com>
2026-01-27 23:33:01 -0600

    SI 0.011

--- research/current/digit_recognizer.py
+++ research/current/digit_recognizer.py
@@ -40,3 +40,16 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(28 * 28, 128) #Extracts raw features(lines/edges)
         self.fc2 = nn.Linear(128, 64) #Combines features into concepts (loops, crossings)
         self.fc3 = nn.Linear(64, 10) #Takes the concepts and makes the final vote (0-9)
+
+        def forward(self, x):
+            x = x.view(-1, 28 * 28)
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = self.fc3(x)
+            return x
+
+net = Net()
+print(net)
+
+criterion = nn.CrossEntropyLoss()
+optimizer =  optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
