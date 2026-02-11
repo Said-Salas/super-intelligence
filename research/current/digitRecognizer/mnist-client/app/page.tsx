c4b06f584964307cc261d66ef9ee86d839e71a43
Said Salas <saidsalastech@gmail.com>
2026-02-10 22:46:46 -0600

    SI 0.026

--- research/current/digitRecognizer/mnist-client/app/page.tsx
+++ research/current/digitRecognizer/mnist-client/app/page.tsx
@@ -1,6 +1,18 @@
-import Image from "next/image";
+'use client'
+
+import { useRef, useState } from 'react'
+import { ReactSketchCanvas, ReactSketchCanvasRef } from 'react-sketch-canvas'
 
 export default function Home() {
+  const canvasRef = useRef<ReactSketchCanvasRef>(null)
+  const [prediction, setPrediction] = useState<{prediction: number; confidence: number} | null>(null)
+  
+  const handlePredict = async () => {
+    if (!canvasRef.current) return
+
+    
+  }
+
   return (
     <div></div>
   );
